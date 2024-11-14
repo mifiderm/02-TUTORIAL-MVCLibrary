da5dc7c47c5a0b6ac128290870691849ff4c0986
Mia Fidermuc Vekić <mifiderm@vsite.hr>
2024-11-14 18:54:01 +0100

    added generated controoler and views.

---
+++ MVCLibrary/Models/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace MVCLibrary.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+    }
+}
